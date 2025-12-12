0d265b0b24fcd3a46da7fdc83f43f81d9f6ba8b0
leo <leo@gmail.com>
2025-12-12 10:44:59 +0200

    refactor Diddy animation into Stimulus controller and update background image to jpg

---
+++ app/javascript/controllers/diddy_controller.js
@@ -0,0 +1,143 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = {
+    spriteUrl: String,
+  }
+
+  connect() {
+    this.canvas = this.element
+    this.ctx = this.canvas.getContext("2d")
+
+    this.sprite = new Image()
+    this.sprite.src = this.spriteUrlValue
+
+    this.frameWidth = 36
+    this.frameHeight = 40
+    this.columns = 12
+    this.walkFrames = 8
+    this.rowRight = 6
+    this.bodyOffset = -8
+    this.innerOffsetX = 8
+
+    this.x = this.canvas.width / 2
+    this.y = this.canvas.height - this.frameHeight - 10
+    this.currentFrame = 0
+    this.direction = "right"
+    this.walking = false
+
+    this.speed = 3
+    this.frameDuration = 80
+    this.lastFrameTime = 0
+
+    this.keys = { left: false, right: false }
+
+    this.onKeyDown = this.onKeyDown.bind(this)
+    this.onKeyUp = this.onKeyUp.bind(this)
+
+    window.addEventListener("keydown", this.onKeyDown)
+    window.addEventListener("keyup", this.onKeyUp)
+
+    this.lastTimestamp = 0
+    this.rafId = requestAnimationFrame(this.loop.bind(this))
+  }
+
+  disconnect() {
+    window.removeEventListener("keydown", this.onKeyDown)
+    window.removeEventListener("keyup", this.onKeyUp)
+
+    if (this.rafId) {
+      cancelAnimationFrame(this.rafId)
+    }
+  }
+
+  onKeyDown(e) {
+    if (e.key === "ArrowLeft") {
+      this.keys.left = true
+      this.direction = "left"
+      this.walking = true
+    } else if (e.key === "ArrowRight") {
+      this.keys.right = true
+      this.direction = "right"
+      this.walking = true
+    }
+  }
+
+  onKeyUp(e) {
+    if (e.key === "ArrowLeft") {
+      this.keys.left = false
+    } else if (e.key === "ArrowRight") {
+      this.keys.right = false
+    }
+
+    if (!this.keys.left && !this.keys.right) {
+      this.walking = false
+      this.currentFrame = 0
+    }
+  }
+
+  loop(timestamp) {
+    const delta = timestamp - this.lastTimestamp
+    this.lastTimestamp = timestamp
+
+    this.update(delta)
+
+    if (this.sprite.complete) {
+      this.draw()
+    }
+
+    this.rafId = requestAnimationFrame(this.loop.bind(this))
+  }
+
+  update(delta) {
+    if (this.keys.left) {
+      this.x -= this.speed
+    }
+    if (this.keys.right) {
+      this.x += this.speed
+    }
+
+    if (this.x < 0) this.x = 0
+    if (this.x > this.canvas.width - this.frameWidth) {
+      this.x = this.canvas.width - this.frameWidth
+    }
+
+    if (this.walking) {
+      this.lastFrameTime += delta
+      if (this.lastFrameTime >= this.frameDuration) {
+        this.lastFrameTime = 0
+        this.currentFrame = (this.currentFrame + 1) % this.walkFrames
+      }
+    }
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+    const sx = (this.currentFrame % this.columns) * this.frameWidth + this.innerOffsetX
+    const sy = this.rowRight * this.frameHeight
+
+    this.ctx.save()
+
+    const dirSign = this.direction === "right" ? 1 : -1
+    this.ctx.translate(this.x + this.frameWidth / 2 + this.bodyOffset * dirSign, this.y)
+
+    if (this.direction === "left") {
+      this.ctx.scale(-1, 1)
+    }
+
+    this.ctx.drawImage(
+      this.sprite,
+      sx,
+      sy,
+      this.frameWidth,
+      this.frameHeight,
+      -this.frameWidth / 2,
+      0,
+      this.frameWidth,
+      this.frameHeight,
+    )
+
+    this.ctx.restore()
+  }
+}
